a31e2f5350863e418981c6b2c3cab29764187478
ahmed abdallah <35036845+AhmedAbd-Allah@users.noreply.github.com>
2019-06-22 10:02:21 +0200

    Update userAuth.js

--- Middlewares/auth/userAuth.js
+++ Middlewares/auth/userAuth.js
@@ -1,3 +1,14 @@
+const encryptionService = require('../../Services/EncryptionService');
+
 module.exports = {
-    
+    userAuth: async function (user){
+        try{
+            
+            let User = await function(user){
+             return user.userRole == "user"   
+            }
+        }
+        catch{
+        }
+    }
 }
